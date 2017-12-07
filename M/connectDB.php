3598b5ba5098839a33f219317da132ef7cf36179
J Couderc <32571260+Kryddd@users.noreply.github.com>
2017-12-07 22:17:26 +0100

    add

---
+++ M/connectDB.php
@@ -0,0 +1,9 @@
+<?php
+	require("./M/config_db.php");
+	if (!isset($link)) {
+		$link = mysqli_connect($host, $loginDB, $passwordDB) 
+		or die ("erreur de connexion :" . mysql_error()); 
+		mysqli_select_db($link, $db) 
+		or die (htmlentities("erreur d'accès à la base :") . $db);
+	}
+?>
